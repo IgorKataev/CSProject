0dad11c592f09c82b89302d577632639d232e474
igorkats34@gmail.com <igorkats34@gmail.com>
2024-02-06 15:58:52 +0100

    added third task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+
 // a = 256 => 26
 // b = 891 => 81
 
@@ -9,9 +10,24 @@
 // int result = firstDigit * 10 + thirdDigit;
 // Console.WriteLine($"В числе {number} => {result}");
 
-int number = 947;
-int secondDigit = number / 10 % 10;
-int thirdDigit = number % 10;
-int result = (int)Math.Pow (secondDigit, thirdDigit);
-// int result = Math.Pow (secondDigit, thirdDigit);
-Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
+// int number = 947;
+// int secondDigit = number / 10 % 10;
+// int thirdDigit = number % 10;
+// int result = (int)Math.Pow (secondDigit, thirdDigit);
+// Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
+
+// 456 =>6
+// 7812 => 8
+// 91 => третьей цифры нет
+
+int number = 925;
+if (number >= 100)
+{
+    int thirdDigitFromEnd = number / 100 % 10;
+    Console.WriteLine($"Третья цифра числа {number} с конца: {thirdDigitFromEnd}");
+}
+        else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+
