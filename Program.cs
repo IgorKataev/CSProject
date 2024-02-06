e12cc4e548e2629d9a705d642a22f2f0e578ea3c
igorkats34@gmail.com <igorkats34@gmail.com>
2024-02-06 15:33:10 +0100

    added second task

--- Program.cs
+++ Program.cs
@@ -3,8 +3,15 @@
 // a = 256 => 26
 // b = 891 => 81
 
-int number = 453;
-int firstDigit = number / 100;
-int thirdGigit = number % 10;
-int result = firstDigit * 10 + thirdGigit;
-Console.WriteLine($"В числе {number} => {result}");
+// int number = 453;
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+// Console.WriteLine($"В числе {number} => {result}");
+
+int number = 947;
+int secondDigit = number / 10 % 10;
+int thirdDigit = number % 10;
+int result = (int)Math.Pow (secondDigit, thirdDigit);
+// int result = Math.Pow (secondDigit, thirdDigit);
+Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
