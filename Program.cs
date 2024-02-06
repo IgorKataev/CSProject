0246b4896ef33b1fc4b340be2f465cdac2b4994f
igorkats34@gmail.com <igorkats34@gmail.com>
2024-02-06 15:04:11 +0100

    added first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// a = 256 => 26
+// b = 891 => 81
+
+int number = 453;
+int firstDigit = number / 100;
+int thirdGigit = number % 10;
+int result = firstDigit * 10 + thirdGigit;
+Console.WriteLine($"В числе {number} => {result}");
